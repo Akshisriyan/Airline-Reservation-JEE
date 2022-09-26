611167118291d99a1571634d81ecc1690a9593f1
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-09-26 13:52:21 +0530

    change

--- src/main/java/com/example/javatest1/HelloServlet.java
+++ src/main/java/com/example/javatest1/HelloServlet.java
@@ -17,11 +17,7 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("text/html");
 
         // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+           }
 
     public void destroy() {
     }
